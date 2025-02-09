54396ecea59fb3c6b710d09af2e3a0fd4f266c57
Muzamil <muzixtra@gmail.com>
2025-02-09 17:53:39 +0500

    Updated portfolio projects

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,30 +1,19 @@
-<<<<<<< HEAD
 import { Container, ContainerSucces } from "./styles";
 import { useForm, ValidationError } from "@formspree/react";
 import { toast, ToastContainer } from "react-toastify";
 import { useEffect, useState } from "react";
 import validator from "validator";
-=======
-import { Container, ContainerSucces } from './styles'
-import { useForm, ValidationError } from '@formspree/react'
-import { toast, ToastContainer } from 'react-toastify'
-
-import { useEffect, useState } from 'react'
-import validator from 'validator'
->>>>>>> 53dfed4d5e08db8f8fc9e612d0009d680195fde3
 
 export function Form() {
   const [state, handleSubmit] = useForm("xknkpqry");
   const [validEmail, setValidEmail] = useState(false);
   const [isHuman, setIsHuman] = useState(false);
   const [message, setMessage] = useState("");
+
   function verifyEmail(email: string) {
-    if (validator.isEmail(email)) {
-      setValidEmail(true);
-    } else {
-      setValidEmail(false);
-    }
+    setValidEmail(validator.isEmail(email));
   }
+
   useEffect(() => {
     if (state.succeeded) {
       toast.success("Email successfully sent!", {
@@ -35,7 +24,8 @@ export function Form() {
         toastId: "succeeded"
       });
     }
-  });
+  }, [state.succeeded]);
+
   if (state.succeeded) {
     return (
       <ContainerSucces>
@@ -50,6 +40,7 @@ export function Form() {
       </ContainerSucces>
     );
   }
+
   return (
     <Container>
       <h2>Get in touch using the form</h2>
@@ -59,43 +50,20 @@ export function Form() {
           id='email'
           type='email'
           name='email'
-          onChange={(e) => {
-            verifyEmail(e.target.value);
-          }}
+          onChange={(e) => verifyEmail(e.target.value)}
           required
         />
         <ValidationError prefix='Email' field='email' errors={state.errors} />
+
         <textarea
           required
           placeholder='Send a message to get started.'
           id='message'
           name='message'
-          onChange={(e) => {
-            setMessage(e.target.value);
-          }}
+          onChange={(e) => setMessage(e.target.value)}
         />
-        {/* <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-<<<<<<< HEAD
-        <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
-          onChange={(e) => {
-            setIsHuman(true)
-          }}
-        ></ReCAPTCHA> */}
-        <button type='submit' disabled={state.submitting || !validEmail || !message || !isHuman}>
-=======
-       
-        <button
-          type="submit"
-          disabled={state.submitting || !validEmail || !message }
-        >
->>>>>>> 53dfed4d5e08db8f8fc9e612d0009d680195fde3
-          Submit
-        </button>
+
+        <button type='submit'>Submit</button>
       </form>
       <ToastContainer />
     </Container>
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,5 @@
 import { Container } from "./styles";
 import githubIcon from "../../assets/github.svg";
-// import DownloadApp from '../../assets/download.png'
 import externalLink from "../../assets/external-link.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
@@ -9,6 +8,7 @@ export function Project() {
     <Container id='project'>
       <h2>My Projects</h2>
       <div className='projects'>
+        {/* Project 1: Daraz Landing Page Clone */}
         <ScrollAnimation animateIn='flipInX'>
           <div className='project'>
             <header>
@@ -28,36 +28,36 @@ export function Project() {
               <div className='project-links'>
                 <a
                   href='https://github.com/muzamilsattar/ecommerce-sale-project'
-                  target='\_blank'
+                  target='_blank'
                   rel='noreferrer'>
-                  <img src={githubIcon} alt='Visit site' />
+                  <img src={githubIcon} alt='GitHub' />
                 </a>
                 <a
                   href='https://ecommerce-sale-project.vercel.app/'
-                  target='\_blank'
+                  target='_blank'
                   rel='noreferrer'>
-                  <img src={externalLink} alt='Visit site' />
+                  <img src={externalLink} alt='Live Demo' />
                 </a>
               </div>
             </header>
             <div className='body'>
               <h3>Daraz Landing Page Clone</h3>
               <p>
-                Developed a highly accurate fully responsive single page web page clone using
-                Tailwind and React-js. The page features a responsive design, smooth animations, and
-                a visually appealing layout.
+                Fully responsive web page clone using Tailwind CSS and React. Features smooth
+                animations and a visually appealing design.
               </p>
             </div>
             <footer>
               <ul className='tech-list'>
-                <li>React-js</li>
+                <li>React.js</li>
                 <li>Tailwind CSS</li>
-                <li>BootStrap</li>
+                <li>Bootstrap</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
+        {/* Project 2: Worldwise TODO List */}
         <ScrollAnimation animateIn='flipInX'>
           <div className='project'>
             <header>
@@ -71,42 +71,39 @@ export function Project() {
                 strokeWidth='1'
                 strokeLinecap='round'
                 strokeLinejoin='round'>
-                {" "}
-                <title>Folder</title>{" "}
-                <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>{" "}
+                <title>Folder</title>
+                <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>
               </svg>
               <div className='project-links'>
                 <a
                   href='https://github.com/muzamilsattar/wolrdwise'
                   target='_blank'
                   rel='noreferrer'>
-                  <img src={githubIcon} alt='Visit site' />
+                  <img src={githubIcon} alt='GitHub' />
                 </a>
-                <a href='https://wolrdwise.vercel.app/' target='_blank' rel='noreferrer'>
-                  <img src={externalLink} alt='Visit site' />
-                </a>{" "}
+                <a href='https://muziworldwise.vercel.app/' target='_blank' rel='noreferrer'>
+                  <img src={externalLink} alt='Live Demo' />
+                </a>
               </div>
             </header>
             <div className='body'>
-              <h3>Worldwise Application for choosing TODO List </h3>
+              <h3>Worldwise App</h3>
               <p>
-                {" "}
-                Developed a WorldWise app using React, enabling users to set their todo City list by
-                clicking on the map and select it for thier trip . A world map that tracks your
-                footsteps into every city you can think of. Never forget your wonderful experiences,
-                and show your friends how you have wandered the world.
+                A React app that lets users track and save city locations for their trip, featuring
+                an interactive map.
               </p>
             </div>
             <footer>
-              {" "}
               <ul className='tech-list'>
-                {" "}
-                <li>React-leaflet Map </li> <li>local json server</li> <li>useContext</li>{" "}
-              </ul>{" "}
+                <li>React-Leaflet Map</li>
+                <li>Local JSON Server</li>
+                <li>useContext</li>
+              </ul>
             </footer>
           </div>
         </ScrollAnimation>
 
+        {/* Project 3: Omni Food Website */}
         <ScrollAnimation animateIn='flipInX'>
           <div className='project'>
             <header>
@@ -116,31 +113,30 @@ export function Project() {
                 role='img'
                 viewBox='0 0 24 24'
                 fill='none'
-                stroke='#23ce6b '
+                stroke='#23ce6b'
                 strokeWidth='1'
                 strokeLinecap='round'
                 strokeLinejoin='round'>
-                <title>Folder</title>{" "}
-                <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>{" "}
+                <title>Folder</title>
+                <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>
               </svg>
               <div className='project-links'>
                 <a
                   href='https://github.com/muzamilsattar/muzifood'
                   target='_blank'
                   rel='noreferrer'>
-                  <img src={githubIcon} alt='Visit site' />{" "}
+                  <img src={githubIcon} alt='GitHub' />
                 </a>
                 <a href='https://muzifood.netlify.app/' target='_blank' rel='noreferrer'>
-                  <img src={externalLink} alt='Visit site' />
+                  <img src={externalLink} alt='Live Demo' />
                 </a>
               </div>
             </header>
             <div className='body'>
-              <h3>Omni Food project in HTML/CSS</h3>
+              <h3>Omni Food Website</h3>
               <p>
-                Developed an interactive website for a food delivery company using HTML, CSS, and
-                JavaScript. The website features a responsive design, smooth animations, and a
-                visually appealing layout.
+                Interactive food delivery website built with HTML, CSS, and JavaScript. Features
+                smooth animations and a beautiful layout.
               </p>
             </div>
             <footer>
@@ -153,71 +149,7 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
-        <ScrollAnimation animateIn='flipInX'>
-          <div className='project'>
-            <header>
-              <svg
-                width='50'
-                xmlns='http://www.w3.org/2000/svg'
-                role='img'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='#23ce6b '
-                stroke-width='1'
-                stroke-linecap='round'
-                stroke-linejoin='round'>
-                <title>Folder</title>
-                <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>
-              </svg>
-              <div className='project-links'>
-<<<<<<< HEAD
-                <a href='https://github.com/muzamilsattar/movies' target='_blank' rel='noreferrer'>
-=======
-                <a href='https://github.com/muzamilsattar/muzamilsattar' target='_blank' rel='noreferrer'>
->>>>>>> 53dfed4d5e08db8f8fc9e612d0009d680195fde3
-                  <img src={githubIcon} alt='Visit site' />
-                </a>
-                <a href='https://movieezverse.vercel.app/' target='_blank' rel='noreferrer'>
-                  <img src={externalLink} alt='Visit site' />
-                </a>
-              </div>
-            </header>
-            <div className='body'>
-<<<<<<< HEAD
-              <h3>Movies Searhing Website in React-Js</h3>
-              <p>
-                {/* movies searching and adding to watch list app in react */}
-                Developed a movie searching and adding to watch list app in React. The app uses the
-                OMDB API to fetch movie data and allows users to add movies to their watch list. The
-                app also features a search bar to filter movies by title and a pagination feature to
-                display a limited number of movies per page.
-=======
-              <h3>OMDb Movie Search and Watchlist App</h3>
-              <p>
-               A React.js app to search for movies using the OMDb API and add them to a personalized watchlist. Discover, 
-                organize, and track your favorite movies effortlessly!
->>>>>>> 53dfed4d5e08db8f8fc9e612d0009d680195fde3
-              </p>
-            </div>
-            <footer>
-              <ul className='tech-list'>
-<<<<<<< HEAD
-                <li>React-Js</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>Fetch API</li>
-=======
-                <li>omdb ID</li>
-                <li>React-js</li>
-              
-                <li>CSS</li>
-                <li>Vercel</li>
->>>>>>> 53dfed4d5e08db8f8fc9e612d0009d680195fde3
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
+        {/* Project 4: Movie Search App */}
         <ScrollAnimation animateIn='flipInX'>
           <div className='project'>
             <header>
@@ -231,38 +163,34 @@ export function Project() {
                 strokeWidth='1'
                 strokeLinecap='round'
                 strokeLinejoin='round'>
-                <title>Code</title>
+                <title>Folder</title>
                 <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>
               </svg>
               <div className='project-links'>
-                <a href='#' target='_blank' rel='noreferrer'>
-                  <img src={githubIcon} alt='Visit site' />
+                <a href='https://github.com/muzamilsattar/movies' target='_blank' rel='noreferrer'>
+                  <img src={githubIcon} alt='GitHub' />
                 </a>
-                <a href='#' target='_blank' rel='noreferrer'>
-                  <img src={externalLink} alt='Visit site' />
+                <a href='https://movieezverse.vercel.app/' target='_blank' rel='noreferrer'>
+                  <img src={externalLink} alt='Live Demo' />
                 </a>
               </div>
             </header>
             <div className='body'>
-              <h3>Real-Time Hand Gesture-Driven 3D Object Manipulation</h3>
+              <h3>Movie Search App</h3>
               <p>
-                Developed a real-time hand gesture recognition system for intuitive 3D object
-                manipulation. Leveraged a multimodal approach integrating OpenCV for video
-                processing, MediaPipe for hand detection/landmark extraction, and scikit-learn for
-                machine learning model training. Achieved 95.2% accuracy in hand gesture
-                classification with efficient real-time performance.
+                A React.js app that fetches movies from the OMDB API, allowing users to search and
+                add movies to their watchlist.
               </p>
             </div>
             <footer>
               <ul className='tech-list'>
-                <li>OpenCV</li>
-                <li>MediaPipe</li>
-                <li>scikit-learn</li>
+                <li>React.js</li>
+                <li>OMDb API</li>
+                <li>CSS</li>
               </ul>
             </footer>
           </div>
         </ScrollAnimation>
-
         <ScrollAnimation animateIn='flipInX'>
           <div className='project'>
             <header>
@@ -272,33 +200,38 @@ export function Project() {
                 role='img'
                 viewBox='0 0 24 24'
                 fill='none'
-                stroke='#23ce6b '
-                stroke-width='1'
-                stroke-linecap='round'
-                stroke-linejoin='round'>
+                stroke='#23ce6b'
+                strokeWidth='1'
+                strokeLinecap='round'
+                strokeLinejoin='round'>
                 <title>Folder</title>
                 <path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path>
               </svg>
               <div className='project-links'>
-                <a href='#' target='_blank' rel='noreferrer'>
+                <a
+                  href='https://github.com/muzamilsattar/pizzaorder'
+                  target='_blank'
+                  rel='noreferrer'>
+                  <img src={githubIcon} alt='Visit site' />
+                </a>
+                <a href='https://reduxpizza.vercel.app/' target='_blank' rel='noreferrer'>
                   <img src={externalLink} alt='Visit site' />
                 </a>
               </div>
             </header>
             <div className='body'>
-              <h3>VeloCityAI</h3>
+              <h3>React Redux Pizza App</h3>
               <p>
-                VelocityAI is a website that allows users to get instant answers to any question and
-                generate stunning AI pictures using OpenAI's GPT-3 and DALL-E model.With VelocityAI,
-                users can type in text-based prompts and receive a corresponding response.
+                Redux Pizza is a modern pizza-ordering web app built with React.js, Redux Toolkit,
+                and Tailwind CSS. It features a sleek, user-friendly interface and smooth state
+                management.
               </p>
             </div>
             <footer>
               <ul className='tech-list'>
-                <li>React</li>
-                <li>Typescript</li>
-                <li>Html</li>
-                <li>css</li>
+                <li>React.js</li>
+                <li>Redux Toolkit</li>
+                <li>Tailwind CSS</li>
               </ul>
             </footer>
           </div>
